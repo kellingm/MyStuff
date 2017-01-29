f0c9c2decf899857ef26f15284ef92fa9dea4300
kellingm <mariekelling@yahoo.com>
2017-01-29 16:43:03 -0500

    started work on first assignment

---
+++ kellingm_assignment1/kellingm_assignment1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace kellingm_assignment1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string firstName, middleInitial, lastName, fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCM;
+
+            System.Console.Write("What is your first name? ");
+            firstName = System.Console.ReadLine();
+
+            System.Console.Write("What is your middle initial? " );
+            middleInitial = System.Console.ReadLine();
+
+            System.Console.Write("What is your last name? ");
+            lastName = System.Console.ReadLine();
+
+            fullName = firstName + middleInitial + lastName; 
+
+            System.Console.Write("What is your height in feet? ");
+            heightFeet = Convert.ToInt32(System.Console.ReadLine());
+
+            System.Console.Write("How many inches beyond your base height in feet are you? ");
+            heightInches = Convert.ToDouble(System.Console.ReadLine());
+
+            totalHeightCM = heightInches * 2.54; 
+
+            System.Console.Write("What us your age? ");
+            age = Convert.ToInt32(System.Console.ReadLine());
+
+            System.Console.Write("Are you a citizen? ");
+            isCitizen = Convert.ToBoolean(System.Console.ReadLine());
+
+            
+           
+       
+
+
+
+
+        }    
+
+    }
+}
